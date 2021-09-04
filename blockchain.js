104ef2dcb7592010557dfd880ce4e6478e9f92c0
Rimma Esheva <32555704+rmmshv@users.noreply.github.com>
2021-09-03 19:24:31 -0700

    transactions and mining rewards added (draft code)

--- blockchain.js
+++ blockchain.js
@@ -1,10 +1,18 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(tx_sender, tx_receiver, amount) {
+        this.tx_sender = tx_sender;
+        this.tx_receiver = tx_receiver;
+        this.amount = amount;
+    }
+
+}
+
 class Block {
-    constructor(index, timestamp, data, prev_hash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, prev_hash = '') {
         this.timestamp =timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.prev_hash = prev_hash;
         this.hash = this.calculate_hash();
         this.nonce = 0;
@@ -27,24 +35,56 @@ class Block {
 class Blockchain{
     constructor(){
         this.chain = [this.generate_genesis()]; // array of blocks, starts with the genesis block
-        this.difficulty = 5; // the higher difficulty value is, the longer it takes to generate new block
+        this.difficulty = 2; // the higher difficulty value is, the longer it takes to generate new block
+        this.mempool = []; // pending transactions
+        this.miner_reward = 42;
     }
     // manualy creating genesis block
     generate_genesis(){
-        return new Block(0, "01/01/2021", "Genesis Block", "0");
+        return new Block("01/01/2021", "Genesis Block", "0");
     }
 
     get_last_block(){
         return this.chain[this.chain.length -1]; // returns last block
     }
     // new block contains hash of the previous block and its own hash
-    add_block(new_block){
-        new_block.prev_hash = this.get_last_block().hash;
-        new_block.mining(this.difficulty)
-        this.chain.push(new_block); // push\add this block to the blockchain
+    mine_pending_txs(miner_reward_address) {
+        let block = new Block(Date.now(), this.mempool);
+        block.mining(this.difficulty);
+
+        console.log('Block is mined successfully.');
+        this.chain.push(block); // add block to the blockchain
+        
+        // reset mempool
+        this.mempool = [new Transaction(null, miner_reward_address, this.miner_reward)];
     }
 
-    // chach if blockchain is valid
+    // add new transactions to mempool
+    create_tx(tx) {
+        this.mempool.push(tx);
+    }
+
+    // check address balance
+    get_balace(address) {
+        let balance = 0;
+
+        for(const block of this.chain) {
+            for (const tx of block.transactions) {
+                // if address belongs to a sender, reduce their balance
+                if(tx.tx_sender == address) {
+                    balance -= tx.amount;
+                }
+                // if adress belongs to receiver of the transaction, increase their balance
+                if(tx.tx_receiver == address) {
+                    balance += tx.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    // chack if blockchain is valid
     is_valid(){
         for (let i = 1; i < this.chain.length; i++) 
         {
@@ -64,27 +104,3 @@ class Blockchain{
         return true;
     }
 }
-
-
-
-// initiate blockchain
-let arbor_coin = new Blockchain();
-
-console.log("block 1: ")
-arbor_coin.add_block(new Block(1, "09/01/2021", {amount: 4}));
-
-console.log("block 2: ")
-arbor_coin.add_block(new Block(2, "09/02/2021", {amount: 10}));
-/*
-console.log(JSON.stringify(arbor_coin, null, 4) + '\n');
-
-console.log("'\n'is blockchain valid?: " + arbor_coin.is_valid() + '\n');
-
-// test is_valid function
-arbor_coin.chain[1].data = {amount:100}; 
-
-console.log("'\n'is blockchain  valid?: " + arbor_coin.is_valid() + '\n');
-
-arbor_coin.chain[1].hash = arbor_coin.chain[1].calculate_hash();
-console.log("'\n'is blockchain  valid?: " + arbor_coin.is_valid() + '\n') ;
-*/
